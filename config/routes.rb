2fa4e4f0644c66c365642e25d5ff6402b6159683
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-19 13:52:30 -0700

    Creating (relatively) static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/code_of_ethics'
+
+  get 'static_pages/events'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
